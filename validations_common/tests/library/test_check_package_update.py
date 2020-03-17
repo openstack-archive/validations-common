c7d577790f72350b5da4e25cb8a86717ec6b7562
Gael Chamoulaud (Strider) <gchamoul@redhat.com>
2020-03-17 11:20:16 +0100

    Remove mock as a test requirement and add PyYAML as requirement.
    Now that we have moved to py36 and later runtimes, we no longer
    need to use the third party mock library and can only use the standard
    library unittest.mock instead.
    
    Change-Id: I95d8ddf3b6746d74d98340132b57d5f25d5450c8
    Signed-off-by: Gael Chamoulaud (Strider) <gchamoul@redhat.com>

--- validations_common/tests/library/test_check_package_update.py
+++ validations_common/tests/library/test_check_package_update.py
@@ -12,8 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from mock import MagicMock
-from mock import patch
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
 from validations_common.library.check_package_update import check_update
 from validations_common.library.check_package_update import get_package_details
