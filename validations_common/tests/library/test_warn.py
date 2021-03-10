50eea5edb7325344a1b4b802470fb7945f03e47b
Jiri Podivin <jpodivin@redhat.com>
2021-03-10 16:22:51 +0100

    Improved test coverage for library/warn.py
    Test of module initiation, presence of required attributes
    and basic yaml processing.
    
    Closes-bug: #1922726
    
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>
    Change-Id: I030e43b6b0c1fbd7d1e42627a60dfe5741d95d40

--- validations_common/tests/library/test_warn.py
+++ validations_common/tests/library/test_warn.py
@@ -20,4 +20,39 @@ from validations_common.tests import base
 class TestWarn(base.TestCase):
     def setUp(self):
         super(TestWarn, self).setUp()
-        self.warn = warn.main
+        self.warn = warn
+        self.module = mock.MagicMock()
+        self.module.params = {'msg': 'foo'}
+
+    @mock.patch(
+        'validations_common.library.warn.AnsibleModule')
+    @mock.patch(
+        'validations_common.library.warn.yaml_safe_load',
+        return_value={'options': mock.MagicMock()})
+    def test_warn_run(self, mock_safe_load, mock_ansible_module):
+        """Verify that warn correctly works with provided YAML.
+        """
+        mock_module = mock.MagicMock()
+        mock_module.params = {'msg': 'foo'}
+        mock_ansible_module.return_value = mock_module
+
+        self.warn.main()
+
+        mock_safe_load.assert_called_once_with(self.warn.DOCUMENTATION)
+        mock_module.exit_json.assert_called_once_with(
+            changed=False,
+            warnings=['foo'])
+
+    def test_warn_attributes(self):
+        """Verify that module contains required attributes.
+        """
+
+        required_names = set(
+            [
+                'DOCUMENTATION',
+                'EXAMPLES',
+                'AnsibleModule'
+            ])
+
+        warn_names = set(dir(self.warn))
+        self.assertTrue(warn_names.issuperset(required_names))
